bbbc3139d0d4e924951ff881fe4589944f4c486a
Aled Jones <aledjones@nhs.net>
2016-11-18 09:28:02 +0000

    first addition of app

---
+++ src/code.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# The following line causes bash to exit at any point if there is any error
+# and to output each line as it is executed -- useful for debugging
+#set -e -x
+
+# Fetch input files
+dx download "$NA12878_vcf" -o NA12878_vcf
+dx download "$bedfile" -o bedfile.bed
+
+
+# Run vcftools
+vcftools --bed  bedfile.bed --vcf NA12878_vcf  --out $bedfile_prefix --recode --recode-INFO-all
+
+# upload filtered vcf and capture the file id
+file_id=$(dx upload $bedfile_prefix.recode.vcf --brief)
+
+#assign to filtered vcf output in JSON
+dx-jobutil-add-output "filtered_vcf"  "$file_id"
+
+
