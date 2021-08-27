81a09ef78ca164aaa9fae571ab8f62c902794ba9
Rachel Duffin <rachel.g.duffin@gmail.com>
2021-08-27 12:42:27 +0100

    Modify code to run bcftools view

--- src/code.sh
+++ src/code.sh
@@ -2,19 +2,17 @@
 
 # The following line causes bash to exit at any point if there is any error
 # and to output each line as it is executed -- useful for debugging
-#set -e -x
+set -e -x -o pipefail
 
 # Fetch input files
 dx download "$input_vcf" -o input_vcf
 dx download "$bedfile" -o bedfile.bed
 
 # Run vcftools
-vcftools --bed  bedfile.bed --vcf input_vcf  --out $bedfile_prefix --recode --recode-INFO-all
+bcftools view input_vcf -R bedfile.bed -O z -o $bedfile_prefix.vcf
 
 # upload filtered vcf and capture the file id
-file_id=$(dx upload $bedfile_prefix.recode.vcf --brief)
+file_id=$(dx upload $input_vcf_prefix.vcf --brief)
 
 #assign to filtered vcf output in JSON
 dx-jobutil-add-output "filtered_vcf"  "$file_id"
-
-
