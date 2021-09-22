b4955299e6d47f6adb566d88b3c8532f43e29ae2
Rachel Duffin <rachel.g.duffin@gmail.com>
2021-09-22 14:25:29 +0100

    Switch from executable to docker image, update documentation

--- src/code.sh
+++ src/code.sh
@@ -1,19 +1,26 @@
 #!/bin/bash
 
-# The following line causes bash to exit at any point if there is any error
-# and to output each line as it is executed -- useful for debugging
-set -e -x -o pipefail
+main () {
+  # The following line causes bash to exit at any point if there is any error
+  # and to output each line as it is executed -- useful for debugging
+  set -e -x -o pipefail
 
-# Fetch input files
-dx download "$input_vcf" -o input_vcf
-dx download "$bedfile" -o bedfile.bed
-dx download "$input_vcf_index" -o input_vcf_index
+  # Fetch input files
+  dx download "$input_vcf" -o input_vcf
+  dx download "$bedfile" -o bedfile.bed
+  dx download "$input_vcf_index" -o input_vcf_index
 
-# Run vcftools
-bcftools view input_vcf##idx##input_vcf_index -R bedfile.bed -O z -o $input_vcf_prefix.vcf.gz
+  # Download and load docker image and call bcftools view. Image is a DNAnexus asset in 001_ToolsReferenceData in
+  # compreseed tarball format.
+  # The docker -v flag mounts a local directory to the docker environment in the format: -v local_dir:docker_dir
+  docker load < project-ByfFPz00jy1fk6PjpZ95F27J:file-G55X9q00jy1ZvJJy4ZGqk2K3
 
-# upload filtered vcf and capture the file id
-file_id=$(dx upload $input_vcf_prefix.vcf.gz --brief)
+  docker run -v /home/dnanexus:/home/dnanexus samtools/bcftools:1.13 view input_vcf##idx##input_vcf_index \
+      -R bedfile.bed -O z -o $input_vcf_prefix.vcf.gz
 
-#assign to filtered vcf output in JSON
-dx-jobutil-add-output "filtered_vcf"  "$file_id"
+  # upload filtered vcf and capture the file id
+  file_id=$(dx upload $input_vcf_prefix.vcf.gz --brief)
+
+  #assign to filtered vcf output in JSON
+  dx-jobutil-add-output "filtered_vcf"  "$file_id"
+  }
