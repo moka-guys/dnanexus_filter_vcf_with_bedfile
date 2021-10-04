930d55932ff2081fe25a9acd34b99e74b2bee0a5
Rachel Duffin <rachel.g.duffin@gmail.com>
2021-10-04 14:22:15 +0100

    Make requested changes from PR

--- Dockerfile
+++ Dockerfile
@@ -8,7 +8,7 @@ LABEL author="Rachel Duffin" \
 RUN apt update && apt install -y libgsl-dev=2.6+dfsg-2 libperl-dev=5.32.1-4+deb11u1 && \
     git clone --recurse-submodules git://github.com/samtools/htslib.git --branch 1.13 
 
-# Install bcftools
+# Install bcftools v1.13
 RUN git clone https://github.com/samtools/bcftools.git --branch 1.13 && \
     cd bcftools && \
     autoheader && autoconf && ./configure --enable-libgsl --enable-perl-filters && \
--- src/code.sh
+++ src/code.sh
@@ -6,22 +6,28 @@ main () {
   set -e -x -o pipefail
 
   # Fetch input files
-  dx download "$input_vcf" -o input_vcf
-  dx download "$bedfile" -o bedfile
-  dx download "$input_vcf_index" -o input_vcf_index
-  # Download docker image
-  dx download project-ByfFPz00jy1fk6PjpZ95F27J:file-G55XqF00jy1QkJ174ZzZfzV5
+  dx-download-all-inputs --parallel
+  # Download docker image, get tag and print
+  DOCKER_FILE_ID=project-ByfFPz00jy1fk6PjpZ95F27J:file-G55XqF00jy1QkJ174ZzZfzV5
+  dx download ${DOCKER_FILE_ID}
+
+  DOCKER_IMAGE_FILE=$(dx describe ${DOCKER_FILE_ID} --name)
+  DOCKER_IMAGE_NAME=$(tar xfO "${DOCKER_IMAGE_FILE}" manifest.json | sed -E 's/.*"RepoTags":\["?([^"]*)"?.*/\1/')
 
   # Create output directory
-  out_dir=out/filtered_vcf/output && mkdir -p ${out_dir}
+  out_dir=/home/dnanexus/out/filtered_vcf/output && mkdir -p ${out_dir}
 
-  # Call bcftools view. Docker image is a DNAnexus asset in 001_ToolsReferenceData in compreseed tarball format.
-  # The docker -v flag mounts a local directory to the docker environment in the format: -v local_dir:docker_dir
-  docker load < bcftools_v1.13.tar.gz
-
-  docker run -v /home/dnanexus:/home/dnanexus samtools/bcftools:1.13 view \
-      /home/dnanexus/input_vcf##idx##/home/dnanexus/input_vcf_index -R /home/dnanexus/bedfile -O z \
-      -o /home/dnanexus/${out_dir}/"$input_vcf_prefix".vcf.gz
+  # Call bcftools view (Bcftools v1.13). Docker image is a DNAnexus asset in 001_ToolsReferenceData in compressed
+  # tarball format.
+  # The docker -v flag mounts a local directory to the docker environment in the format: -v local_dir:docker_dir.
+  # -R = supplies file containing regions to restrict the input file.  a file containing regions to restrict the input
+           #file on. Returns all positions overlapping the regions specified in the bed file. Therefore indels that
+           # cover both inside and outside a region are returned.
+  # -O z = output type compressed vcf file
+  docker load < /home/dnanexus/"${DOCKER_IMAGE_FILE}"
+  echo "Using docker image ${DOCKER_IMAGE_NAME}"
+  docker run -v /home/dnanexus:/home/dnanexus "${DOCKER_IMAGE_NAME}" view "$vcf_file_path"##idx##"$vcf_index_path" \
+      -R "$bedfile_path" -O z -o ${out_dir}/"$vcf_file_prefix".vcf.gz
 
   # Create output directory, move output file into directory, and upload outputs
   # dx-upload-all-outputs uploads contents of the subdirectories on the path $HOME/out/
